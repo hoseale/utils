7ecc57676c8bcc2fbe3e2d67c2bb3b559bef0204
hoseaLe <33473651+HoseaLE@users.noreply.github.com>
2023-10-23 16:55:21 +0800

    Update linkAsync.ts

--- src/linkAsync.ts
+++ src/linkAsync.ts
@@ -1,18 +1,34 @@
-// 数组链式调用
-async function linkAsync(arr, cb) {
-	try {
-		const len = arr.length;
-		let index = 0;
-		const hand = async (ls) => {
-			cb && await cb(ls[index]);
-			if (index < len - 1) {
-				index++;
-				await hand(ls)
-			}
-		}
-		await hand(arr)
-	} catch (error) {
+/**
+ * 数组异步遍历
+ * @param data
+ * @param callback 回调函数，回传当前遍历项及下标，next函数调用执行下一项
+ *
+ */
+type AsyncEachParam<T = any, P = any> = {
+  callback: (current: T, index: number, next: (customData?: P) => Promise<void>, customData?: P) => Promise<void>;
+  data: T[];
+};
+export async function asyncEach<T = any, P = any>(
+  data: AsyncEachParam<T, P>['data'],
+  callback: AsyncEachParam<T, P>['callback'],
+): Promise<void> {
+  const len = data.length;
+  const dispatch = async (index: number, customData?: P) => {
+    if (index >= len) {
+      return;
+    }
+    const current = data[index];
+    await callback(
+      current,
+      index,
+      async (cData) => {
+        await dispatch(index + 1, cData);
+      },
+      customData,
+    );
+  };
 
-	}
-
+  if (data.length) {
+    await dispatch(0);
+  }
 }
